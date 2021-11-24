414f66aa0ca1eb6f16b4cae772ca97a0840b5ac6
SuHyun <jish2689@gmail.com>
2021-11-24 22:42:40 +0900

    google 소셜 로그인 기능 완료. API 서비스 만들기 (Delete 테스트 미완)

--- src/main/java/org/zerock/club/config/SecurityConfig.java
+++ src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -1,17 +1,25 @@
 package org.zerock.club.config;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.zerock.club.security.handler.ClubLoginSuccessHandler;
+import org.zerock.club.security.service.ClubUserDetailsService;
 
 @Configuration
 @Log4j2
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private ClubUserDetailsService userDetailsService;
 
     @Bean
     PasswordEncoder passwordEncoder(){
@@ -21,14 +29,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.authorizeRequests().antMatchers("/sample/all").permitAll()
-                .antMatchers("/sample/member").hasRole("USER");
+        //http.authorizeRequests().antMatchers("/sample/all").permitAll()
+        //        .antMatchers("/sample/member").hasRole("USER");
         
         http.formLogin(); // 인가/인증 문제 시 로그인 화면 표출
         http.csrf().disable(); // csrf 토큰 생성 비활성화 -> 외부에서 REST 방식으로 이용하는 보안 설정 다루기 위해
+        http.oauth2Login().successHandler(successHandler()); // google login
         http.logout(); // 로그아웃
+        http.rememberMe().tokenValiditySeconds(60*60*7).userDetailsService(userDetailsService); // rememberMe 유효기간 7일
     }
 
+    @Bean
+    public ClubLoginSuccessHandler successHandler(){
+        return new ClubLoginSuccessHandler(passwordEncoder());
+    }
 
 
     /*
